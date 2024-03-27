d2ff82392d9c9f586818283edb8438f897464603
Daniel Vazquez <daavo.1002@gmail.com>
2024-03-27 00:31:21 -0600

    Login

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import users, register
+from .views import users, register, login
 
 urlpatterns = [
     path('users', users),
     path('register', register),
+    path('login', login),
 ]
