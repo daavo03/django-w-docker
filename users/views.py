df68b3e99ea20823b14ac605ce1fb934ab1b9bbc
Daniel Vazquez <daavo.1002@gmail.com>
2024-03-27 00:41:30 -0600

    Generate JWT Token

--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from rest_framework import exceptions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .authentication import generate_access_token
+
 from .serializers import UserSerializer
 from .models import User
 
@@ -29,7 +31,15 @@ def login(request):
   if not user.check_password(password):
     raise exceptions.AuthenticationFailed("Incorrect password!")
   
-  return Response("success")
+  response = Response()
+
+  token = generate_access_token(user)
+  response.set_cookie(key="jwt", value=token, httponly=True)
+  response.data = {
+    "jwt": token
+  }
+  
+  return response
 
 @api_view(["GET"])
 def users(request):
