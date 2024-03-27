1256b74c2fa24bd25a12ce8570d40d113760a3ed
Daniel Vazquez <daavo.1002@gmail.com>
2024-03-26 23:04:30 -0600

    Users registration & serializer

--- users/views.py
+++ users/views.py
@@ -1,6 +1,22 @@
+from rest_framework import exceptions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .serializers import UserSerializer
 from .models import User
+
+@api_view(["POST"])
+def register(request):
+  data = request.data
+
+  if data["password"] != data["password_confirm"]:
+    raise exceptions.APIException("Passwords do not match!")
+  
+  serializer = UserSerializer(data=data)
+  serializer.is_valid(raise_exception=True)
+  serializer.save()
+
+  return Response(serializer.data)
 
 @api_view(["GET"])
 def users(request):
