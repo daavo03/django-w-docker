a8af600b81a3535c5092ba45d2b50171191f9c0b
Daniel Vazquez <daavo.1002@gmail.com>
2024-03-27 00:53:39 -0600

    Getting the Authenticated User

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from rest_framework import exceptions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
-from .authentication import generate_access_token
+from .authentication import JWTAuthentication, generate_access_token
 
 from .serializers import UserSerializer
 from .models import User
@@ -40,6 +42,19 @@ def login(request):
   }
   
   return response
+
+class AuthenticatedUser(APIView):
+  authentication_classes = [JWTAuthentication]
+  permission_classes = [IsAuthenticated]
+
+  def get(self, request):
+    serializer = UserSerializer(request.user)
+
+    return Response({
+      "data": serializer.data
+    })
+
+
 
 @api_view(["GET"])
 def users(request):
